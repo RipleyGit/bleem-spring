f6d8feba7f4d2246ead84185bba3dfebf0d8ab40
yubangshui <yubangshui@njbestway.com>
2024-01-03 16:34:24 +0800

    feat:RESTful接口设计规范.md

--- src/main/java/site/bleem/boot/demo/controller/RestfulController.java
+++ src/main/java/site/bleem/boot/demo/controller/RestfulController.java
@@ -1,35 +1,13 @@
 package site.bleem.boot.demo.controller;
 
-import org.apache.commons.io.FileUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.core.io.support.ResourcePatternUtils;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.SerializationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import site.bleem.boot.demo.controller.dto.QueryDTO;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.stream.XMLEventReader;
-import javax.xml.stream.XMLEventWriter;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLOutputFactory;
-import javax.xml.stream.events.XMLEvent;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URLEncoder;
-
 /**
  * @author yubs
  * @desc restful接口格式
- * @date 2023/12/15
  */
 @RestController
 public class RestfulController {
